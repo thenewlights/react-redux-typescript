f74e0d74e49bec0a3d20e5e526233645541eca78
Eugene <myeamil@mail.com>
2023-06-23 16:16:49 +0300

    fixed dispatch

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,11 +1,29 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
 import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store/action-creators/user";
+import { Dispatch } from "redux";
 
 const UserList: React.FC = () => {
   const { users, error, loading } = useTypedSelector((state) => state.user);
+  const dispatch: Dispatch<any> = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+  if (loading) {
+    return <h1>Идет загрузка...</h1>;
+  }
+  if (error) {
+    return <h1>{error}</h1>;
+  }
 
-  return <div></div>;
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default UserList;
